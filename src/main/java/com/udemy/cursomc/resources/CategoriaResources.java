25e4be9b1e708d297bf5c1089e772cc0973087dc
Fernando Coutinho <fa2c1@hotmail.com>
2020-12-28 22:10:25 -0300

    Testando a pimeira classe de dominio

--- src/main/java/com/udemy/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/udemy/cursomc/resources/CategoriaResources.java
@@ -1,16 +1,29 @@
 package com.udemy.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.udemy.cursomc.domain.Categorias;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "REST esta funcionando";
+	public List<Categorias> Listar() {
+		
+		Categorias cat1 = new Categorias(1, "Informatica");
+		Categorias cat2 = new Categorias(2, "Escritorio");
+		
+		List<Categorias> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
