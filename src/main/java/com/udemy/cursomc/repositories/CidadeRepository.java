bab2aabc94914eaa3b8143a03efd0bce583c97e7
Fernando Coutinho <fa2c1@hotmail.com>
2020-12-30 14:54:23 -0300

    Estado e cidade

--- src/main/java/com/udemy/cursomc/repositories/CidadeRepository.java
+++ src/main/java/com/udemy/cursomc/repositories/CidadeRepository.java
@@ -3,10 +3,9 @@ package com.udemy.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.udemy.cursomc.domain.Categoria;
-
+import com.udemy.cursomc.domain.Cidade;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
 
 }
--- src/main/java/com/udemy/cursomc/repositories/EstadoRepository.java
+++ src/main/java/com/udemy/cursomc/repositories/EstadoRepository.java
@@ -3,10 +3,9 @@ package com.udemy.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.udemy.cursomc.domain.Categoria;
-
+import com.udemy.cursomc.domain.Estado;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
 
 }
